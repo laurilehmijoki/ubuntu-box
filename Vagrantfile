bbe7d5ba240e2cd4fe6a4bbfc0fb41ae46b41ed3
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-04-20 22:42:20 +0300

    Vagrant config for devel Ubuntu

---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+Vagrant::Config.run do |config|
+  config.vm.box = "base"
+
+  config.vm.network :bridged
+
+  config.vm.provision :shell, :path => "vagrant-init.sh"
+
+  # Share an additional folder to the guest VM. The first argument is
+  # an identifier, the second is the path on the guest to mount the
+  # folder, and the third is the path on the host to the actual folder.
+  # config.vm.share_folder "v-data", "/vagrant_data", "../data"
+end
---
+++ vagrant-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Installing Debian packages...
+packages=( git-core ruby ruby-dev rubygems rake vim-nox curl tmux ctags ack-grep build-essential )
+for package in ${packages[@]}
+do
+  echo "Ensuring that package $package exists..."
+  sudo apt-get install -y ${package} > /dev/null
+done
+
+echo Cloning dotfiles from GitHub as read-only
+git init
+git remote add origin git://github.com/laurilehmijoki/dotfiles.git
+git pull origin master
+source .bash_profile
+
+echo Git is now set up. For read-write access, remember to add the public SSH key to GitHub.
