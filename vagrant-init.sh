e315cd0bd08cf3ad074148962a906b37119d4c7c
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-04-21 19:17:14 +0300

    set locale

--- vagrant-init.sh
+++ vagrant-init.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 echo Installing Debian packages...
+apt-get update
 packages=( git-core ruby ruby-dev rubygems rake vim-nox curl tmux ctags ack-grep build-essential )
 for package in ${packages[@]}
 do
   echo "Ensuring that package $package exists..."
-  sudo apt-get install -y ${package} > /dev/null
+  apt-get install -y ${package} > /dev/null
 done
 
-echo Cloning dotfiles from GitHub as read-only
+echo Cloning dotfiles from GitHub as read-only...
 git init
 git remote add origin git://github.com/laurilehmijoki/dotfiles.git
 git pull origin master
-
 echo Git is now set up. For read-write access, remember to add the public SSH key to GitHub.
+
+echo Setting the locale...
+echo LC_ALL=\"en_US.utf8\" >> /etc/environment
