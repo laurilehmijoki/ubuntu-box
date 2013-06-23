e77b1f5a7a83861b7f57fbc1a4c109efad85c2b0
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-06-23 16:41:52 +0300

    Remove .bashrc and .bash_profile before installing dotfiles

--- modules/coding_environment/files/install_dotfiles.sh
+++ modules/coding_environment/files/install_dotfiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+rm -f .bashrc # git will fail on override
+rm -f .bash_profile # git will fail on override
 git init
 git remote add origin https://github.com/laurilehmijoki/dotfiles.git
 git pull origin master
