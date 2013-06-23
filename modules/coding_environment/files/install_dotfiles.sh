e81854bef537182fb9ee400d57876dae34124759
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-06-23 16:42:45 +0300

    Start tracking dotfiles

--- modules/coding_environment/files/install_dotfiles.sh
+++ modules/coding_environment/files/install_dotfiles.sh
@@ -4,4 +4,4 @@ rm -f .bashrc # git will fail on override
 rm -f .bash_profile # git will fail on override
 git init
 git remote add origin https://github.com/laurilehmijoki/dotfiles.git
-git pull origin master
+git pull -u origin master
